92ad6b417c7888116f037f42fd4292180df70af9
DidinAmarudinn <amardidin5@gmail.com>
2020-05-27 22:00:52 +0700

    Initial commit

---
+++ app/src/main/java/com/example/encryptionanddecrytionaes/Activitites/Encryption_Act.java
@@ -0,0 +1,16 @@
+package com.example.encryptionanddecrytionaes.Activitites;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.encryptionanddecrytionaes.R;
+
+public class Encryption_Act extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_encryption_);
+    }
+}
---
+++ app/src/main/java/com/example/encryptionanddecrytionaes/Activitites/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.encryptionanddecrytionaes.Activitites;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.encryptionanddecrytionaes.R;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btn_mulai;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn_mulai=findViewById(R.id.btn_mulai);
+
+        btn_mulai.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,Encryption_Act.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
